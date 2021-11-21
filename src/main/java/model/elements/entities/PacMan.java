595e3cfb0cc7202bd1475cb4c1893781aaef2e48
tom.david-broglio <tom.david-broglio@etu.univ-amu.fr>
2021-11-21 22:22:55 +0100

    rotation de PacMan en fonction de sa direction

--- src/main/java/model/elements/entities/PacMan.java
+++ src/main/java/model/elements/entities/PacMan.java
@@ -1,8 +1,12 @@
 package model.elements.entities;
 
+import config.pacman.PacManConfiguration;
 import fr.r1r0r0.deltaengine.model.Coordinates;
 import fr.r1r0r0.deltaengine.model.Dimension;
+import fr.r1r0r0.deltaengine.model.Direction;
 import fr.r1r0r0.deltaengine.model.elements.entity.Entity;
+import fr.r1r0r0.deltaengine.model.events.AttributeListener;
+import fr.r1r0r0.deltaengine.model.sprites.Sprite;
 
 import static config.pacman.PacManConfiguration.CONF_PACMAN_NAME;
 import static config.pacman.PacManConfiguration.CONF_PACMAN_SPRITE;
@@ -10,12 +14,14 @@ import static config.pacman.PacManConfiguration.CONF_PACMAN_SPRITE;
 public class PacMan extends Entity {
 
     private boolean isEnergized;
+    private Direction current;
 
     public PacMan(Coordinates<Double> coordinates) {
         // TODO super("PacMan", coordinates, Image.PAC_MAN.getSprite(), new Dimension(1, 1));
         super(CONF_PACMAN_NAME, coordinates, CONF_PACMAN_SPRITE, new Dimension(0.9, 0.9));
 
         isEnergized = false;
+
     }
 
     public PacMan(double x, double y) {
@@ -37,5 +43,29 @@ public class PacMan extends Entity {
 
     public void setEnergized(boolean isEnergized) {
         this.isEnergized = isEnergized;
+    }
+
+    @Override
+    public Sprite getSprite(){
+        Sprite rotated = CONF_PACMAN_SPRITE;
+        switch (this.getAttributes().getDirection()){
+            case UP:
+                rotated.getNode().setScaleX(1);
+                rotated.getNode().setRotate(-90);
+                break;
+            case DOWN:
+                rotated.getNode().setScaleX(1);
+                rotated.getNode().setRotate(90);
+                break;
+            case RIGHT:
+                rotated.getNode().setScaleX(1);
+                rotated.getNode().setRotate(0);
+                break;
+            case LEFT:
+                rotated.getNode().setScaleX(-1);
+                rotated.getNode().setRotate(0);
+                break;
+        }
+        return rotated;
     }
 }
