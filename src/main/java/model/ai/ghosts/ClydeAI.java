0b41bc903d5b5b5b315da89e5e34a41bc14f6239
Valorator <vidal.guillaume.1@etu.univ-amu.fr>
2021-11-22 09:53:52 +0100

    comment

--- src/main/java/model/ai/ghosts/ClydeAI.java
+++ src/main/java/model/ai/ghosts/ClydeAI.java
@@ -1,8 +1,10 @@
 package model.ai.ghosts;
 
+import fr.r1r0r0.deltaengine.exceptions.NotInitializedException;
 import fr.r1r0r0.deltaengine.model.Coordinates;
 import fr.r1r0r0.deltaengine.model.Direction;
 import fr.r1r0r0.deltaengine.model.elements.CollisionPositions;
+import fr.r1r0r0.deltaengine.model.engines.DeltaEngine;
 import fr.r1r0r0.deltaengine.model.maplevel.MapLevel;
 import model.elements.entities.ghosts.Ghost;
 
@@ -10,7 +12,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * TODO
+ * An AI corresponding to the orange ghost in the game PacMan
+ * His movement are randomly choose, but he can only change direction when he reach a junction
  */
 public final class ClydeAI extends GhostAI {
 
@@ -23,6 +26,9 @@ public final class ClydeAI extends GhostAI {
     private Coordinates<Integer> target;
     private Direction direction;
 
+    /**
+     * Constructor
+     */
     public ClydeAI () {
         random = new Random();
         target = null;
@@ -35,10 +41,18 @@ public final class ClydeAI extends GhostAI {
         else offensiveTick(ghost);
     }
 
+    /**
+     * Action when the ghost is in scary mode
+     * @param ghost a ghost
+     */
     private void scaryTick (Ghost ghost) {
         //TODO
     }
 
+    /**
+     * Action when the ghost is in offensive mode (when he is not in scary mode)
+     * @param ghost a ghost
+     */
     private void offensiveTick (Ghost ghost){
         if (target == null || isTargetReach(ghost)) {
             direction = chooseDirection(ghost);
@@ -47,6 +61,11 @@ public final class ClydeAI extends GhostAI {
         }
     }
 
+    /**
+     * Choose and return the next direction to follow
+     * @param ghost a ghost
+     * @return a direction
+     */
     private Direction chooseDirection (Ghost ghost) {
         MapLevel mapLevel = ghost.getMapLevel();
         Coordinates<Double> position = ghost.getCoordinates();
@@ -64,6 +83,11 @@ public final class ClydeAI extends GhostAI {
         return (size == 0) ? Direction.IDLE : directions.get(random.nextInt(size));
     }
 
+    /**
+     * Select and return the target, the next coordinates where the ghost must go
+     * @param ghost a ghost
+     * @return a coordinates
+     */
     private Coordinates<Integer> selectTarget (Ghost ghost) {
         if (direction == Direction.IDLE) return null;
         MapLevel mapLevel = ghost.getMapLevel();
@@ -78,7 +102,17 @@ public final class ClydeAI extends GhostAI {
         return new Coordinates<>(x - directionCoordinate.getX(), y - directionCoordinate.getY());
     }
 
+    /**
+     * Return if the target is reach
+     * @param ghost a ghost
+     * @return if the target is reach
+     */
     private boolean isTargetReach (Ghost ghost) {
+        try {
+            if ( ! DeltaEngine.getKernelEngine().isAvailableDirection(ghost,direction)) return true;
+        } catch (NotInitializedException e) {
+            //e.printStackTrace();
+        }
         Coordinates<Double> topLeft = ghost.getCoordinates();
         Coordinates<Double> rightBot = CollisionPositions.RIGHT_BOT.calcPosition(topLeft,ghost.getDimension());
         return topLeft.getX().intValue() == rightBot.getX().intValue()
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -14,6 +14,7 @@ import fr.r1r0r0.deltaengine.model.events.InputEvent;
 import fr.r1r0r0.deltaengine.model.events.Trigger;
 import fr.r1r0r0.deltaengine.model.maplevel.MapLevel;
 import fr.r1r0r0.deltaengine.model.maplevel.MapLevelBuilder;
+import fr.r1r0r0.deltaengine.model.sprites.Sprite;
 import fr.r1r0r0.deltaengine.model.sprites.shapes.Circle;
 import fr.r1r0r0.deltaengine.model.sprites.shapes.Rectangle;
 import fr.r1r0r0.deltaengine.view.colors.Color;
@@ -30,42 +31,45 @@ public class Main {
         deltaEngine.setFrameRate(60);
         deltaEngine.printFrameRate(true);
 
-        MapLevel mapLevel = new MapLevelBuilder("test",30,20).build();
-        deltaEngine.addMap(mapLevel);
-        //mapLevel = createMapLevelDamier("test2", 10,10);
-        mapLevel = createMapLevelPrison("test2", 10,10,6,8,6,8);
+
+        MapLevel mapLevel;
+        //mapLevel = createMapLevelDamier("mapTest", 10,10);
+        mapLevel = createMapLevelPrison("mapTest", 10, 10, 6, 8, 6, 8);
         mapLevel.replaceCell(new Wall(7, 7));
-
-        Wall w =new Wall(2,2);
-        mapLevel.replaceCell(new CrossableCell(2,2,new Rectangle(Color.RED)));
         deltaEngine.addMap(mapLevel);
 
-        deltaEngine.setCurrentMap("test");
-
-
+        /*
         Entity pacman = new Entity("pacman", new Coordinates<>(5., 5.), new Circle(1, Color.YELLOW), new Dimension(1, 1));
         pacman.setSpeed(0);
         mapLevel.addEntity(pacman);
+        */
 
-        Entity red = new Entity("red", new Coordinates<>(8., 8.), new Rectangle(0.5,0.5,Color.RED), new Dimension(0.8, 0.8));
+        Entity red = new Entity("red", new Coordinates<>(8., 8.), new Rectangle(0.5, 0.5, Color.RED), new Dimension(0.8, 0.8)) {
+            Sprite p1 = new Rectangle(0.5, 0.5, Color.GREEN);
+            Sprite p2 = new Rectangle(0.5, 0.5, Color.RED);
+            @Override
+            public Sprite getSprite () {
+                return (getCoordinates().getX() < 7) ? p1 : p2;
+            }
+        };
         red.setSpeed(0);
+        mapLevel.addEntity(red);
 
-
+        /*
         InputEvent moveUpEvent = new InputEvent(new ChangeMove(pacman, Direction.UP), new ChangeMove(pacman, Direction.IDLE));
         InputEvent moveDownEvent = new InputEvent(new ChangeMove(pacman, Direction.DOWN), new ChangeMove(pacman, Direction.IDLE));
         InputEvent moveLeftEvent = new InputEvent(new ChangeMove(pacman, Direction.LEFT), new ChangeMove(pacman, Direction.IDLE));
         InputEvent moveRightEvent = new InputEvent(new ChangeMove(pacman, Direction.RIGHT), new ChangeMove(pacman, Direction.IDLE));
+        deltaEngine.setInput(Key.Z, moveUpEvent);
+        deltaEngine.setInput(Key.Q, moveLeftEvent);
+        deltaEngine.setInput(Key.S, moveDownEvent);
+        deltaEngine.setInput(Key.D, moveRightEvent);
+        */
 
         InputEvent moveUpEventRed = new InputEvent(new ChangeMove(red, Direction.UP), new ChangeMove(red, Direction.IDLE));
         InputEvent moveDownEventRed = new InputEvent(new ChangeMove(red, Direction.DOWN), new ChangeMove(red, Direction.IDLE));
         InputEvent moveLeftEventRed = new InputEvent(new ChangeMove(red, Direction.LEFT), new ChangeMove(red, Direction.IDLE));
         InputEvent moveRightEventRed = new InputEvent(new ChangeMove(red, Direction.RIGHT), new ChangeMove(red, Direction.IDLE));
-
-        deltaEngine.setInput(Key.Z, moveUpEvent);
-        deltaEngine.setInput(Key.Q, moveLeftEvent);
-        deltaEngine.setInput(Key.S, moveDownEvent);
-        deltaEngine.setInput(Key.D, moveRightEvent);
-
         deltaEngine.setInput(Key.ARROW_UP, moveUpEventRed);
         deltaEngine.setInput(Key.ARROW_LEFT, moveLeftEventRed);
         deltaEngine.setInput(Key.ARROW_DOWN, moveDownEventRed);
@@ -73,38 +77,20 @@ public class Main {
 
         InputEvent exitEvent = new InputEvent(() -> System.exit(0), null);
         deltaEngine.setInput(Key.ESCAPE, exitEvent);
-
         InputEvent pauseEvent = new InputEvent(deltaEngine::haltCurrentMap, deltaEngine::resumeCurrentMap);
         deltaEngine.setInput(Key.P, pauseEvent);
 
-        /*
-        pacman.setCollisionEvent(red, new Event() {
-            @Override
-            public void checkEvent() {
-                System.out.println("VIDAL E TRO FOR");
-            }
-        });
-        */
-        //pacman.setAI(new BasicAI(pacman));
-
-        deltaEngine.setCurrentMap("test2");
-
-        mapLevel.addEntity(red);
-
-
         Ghost ghost = Ghosts.CLYDE.build(mapLevel);
         ghost.setCoordinates(new Coordinates<>(8., 8.));
-        ghost.setSpeed(30);
-        mapLevel.addEntity(ghost);
+        ghost.setSpeed(10);
+        //mapLevel.addEntity(ghost);
 
-
+        deltaEngine.setCurrentMap("mapTest");
 
         try {
 
-            for (; ; Thread.sleep(1000/60)) {
-                ghost.getAI().tick();
-                //System.out.println(ghost.getCoordinates());
-                //System.out.println(pacman.getCoordinates() + " -- " + red.getCoordinates());
+            for (; ; Thread.sleep(1000)) {
+                //
             }
 
         } catch (Exception e) {
@@ -112,14 +98,14 @@ public class Main {
         }
     }
 
-    private static MapLevel createMapLevelDamier (String name, int width, int height) {
+    private static MapLevel createMapLevelDamier(String name, int width, int height) {
         //TODO: faire une erreur generique pour les MapLevel, qui se subdivise en toutes les erreurs actuels
-        MapLevelBuilder mapLevelBuilder = new MapLevelBuilder(name,width,height);
+        MapLevelBuilder mapLevelBuilder = new MapLevelBuilder(name, width, height);
         try {
             for (int i = 0; i < width; i++) {
                 for (int j = 0; j < height; j++) {
                     mapLevelBuilder.setCell(
-                            new CrossableCell(i, j,new Rectangle(((i + j) & 1) == 0 ? Color.BLACK : Color.WHITE))
+                            new CrossableCell(i, j, new Rectangle(((i + j) & 1) == 0 ? Color.BLACK : Color.WHITE))
                     );
                 }
             }
@@ -129,10 +115,10 @@ public class Main {
         return mapLevelBuilder.build();
     }
 
-    private static MapLevel createMapLevelPrison (String name, int width, int height,
-                                                  int prisonXMin, int prisonXMax,
-                                                  int prisonYMin, int prisonYMax) {
-        MapLevelBuilder mapLevelBuilder = new MapLevelBuilder(name,width,height);
+    private static MapLevel createMapLevelPrison(String name, int width, int height,
+                                                 int prisonXMin, int prisonXMax,
+                                                 int prisonYMin, int prisonYMax) {
+        MapLevelBuilder mapLevelBuilder = new MapLevelBuilder(name, width, height);
         try {
             for (int i = 0; i < width; i++) {
                 for (int j = 0; j < height; j++) {
@@ -152,41 +138,23 @@ public class Main {
         return mapLevelBuilder.build();
     }
 
-}
 
-class ChangeMove implements Trigger {
+    public static class ChangeMove implements Trigger {
 
-    private Direction direction;
-    private Entity e;
+        private Direction direction;
+        private Entity e;
 
-    public ChangeMove(Entity e, Direction direction) {
-        this.direction = direction;
-        this.e = e;
-    }
+        public ChangeMove(Entity e, Direction direction) {
+            this.direction = direction;
+            this.e = e;
+        }
 
-    @Override
-    public void trigger() {
-        e.setSpeed(1);
-        e.setDirection(direction);
-    }
-}
-
-class BasicAI extends AI {
-
-    private Entity entity;
-
-    public BasicAI (Entity entity) {
-        this.entity = entity;
-        this.entity.setDirection(Direction.UP);
-        this.entity.setSpeed(5);
-    }
-    @Override
-    public void tick() {
-        Coordinates<Double> coordinates = entity.getCoordinates();
-        if (coordinates.getY() >= 8) {
-            entity.setDirection(Direction.UP);
-        } else if (coordinates.getY() <= 4) {
-            entity.setDirection(Direction.DOWN);
+        @Override
+        public void trigger() {
+            e.setSpeed(1);
+            e.setDirection(direction);
         }
     }
+
 }
+
