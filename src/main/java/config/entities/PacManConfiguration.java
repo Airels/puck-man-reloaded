c3b48d56ae0c15379a918a18288918783cd51836
Airels <33286314+Airels@users.noreply.github.com>
2021-12-02 13:56:27 +0100

    Refactor affichage sprite PacMan

--- src/main/java/config/entities/PacManConfiguration.java
+++ src/main/java/config/entities/PacManConfiguration.java
@@ -12,6 +12,6 @@ public final class PacManConfiguration {
     public final static double CONF_PACMAN_SPEED = 5;
     public final static Coordinates<Double> CONF_DEFAULT_PACMAN_POSITION = new Coordinates<>(9.05, 16.05);
 
-    public final static Sprite CONF_PACMAN_SPRITE = Image.PAC_MAN.getSprite();
-    public final static Sprite CONF_PACMAN_DEAD_SPRITE = Image.PAC_MAN_DEATH.getSprite();
+    public final static Image CONF_PACMAN_SPRITE = Image.PAC_MAN;
+    public final static Image CONF_PACMAN_DEAD_SPRITE = Image.PAC_MAN_DEATH;
 }
